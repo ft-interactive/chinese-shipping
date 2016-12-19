3cae2383d45e9faddf2389d5eb6b33f9defa3448
digitalcampbell <christopher.campbell@ft.com>
2016-12-19 17:20:47 +0000

    Title and byline tweaks

--- config/article.js
+++ config/article.js
@@ -11,14 +11,14 @@ export default () => ({ // eslint-disable-line
   //       new Date('2016-05-17T17:11:22Z')
   publishedDate: new Date(),
 
-  headline: 'Politics and the English Language',
+  headline: 'China rules the waves',
 
   // summary === standfirst (Summary is what the content API calls it)
   summary: 'Political language is designed to make lies sound truthful' +
            'and murder respectable, and to give an appearance of solidity to pure wind',
 
   topic: {
-    name: 'Starter Kit',
+    name: 'Shipping',
     url: '/foo',
   },
 
@@ -38,7 +38,7 @@ export default () => ({ // eslint-disable-line
   // Byline can by a plain string, markdown, or array of authors
   // if array of authors, url is optional
   byline: [
-    { name: 'Author One', url: '/foo/bar' },
+    { name: 'James Kynge', url: '/foo/bar' },
     { name: 'Author Two' },
   ],
 
